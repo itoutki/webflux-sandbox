9911001a7bf4e5e77e2176d2049ac168441bb2d9
itoutki <itoutki@nttdata.co.jp>
2020-05-19 15:59:45 +0900

    update

--- webflux-sandbox/src/main/java/com/example/webflux/sandbox/thymeleaf/ThymeleafController.java
+++ webflux-sandbox/src/main/java/com/example/webflux/sandbox/thymeleaf/ThymeleafController.java
@@ -18,7 +18,11 @@ public class ThymeleafController {
     public String index(@RequestParam int n, Model model) {
         Flux<Account> accounts = Flux.range(0, n)
                 .map(i -> new Account(String.valueOf(i)));
+        Flux<Account> anotherAccounts = Flux.zip(Flux.range(n, n), Flux.interval(Duration.ofMillis(200)))
+                .map(T -> new Account(String.valueOf(T.getT1())));
         model.addAttribute("accounts", accounts);
+        model.addAttribute("anothers", anotherAccounts);
+
         return "biglist";
     }
 
@@ -26,8 +30,11 @@ public class ThymeleafController {
     public String dd(@RequestParam int n, Model model) {
         Flux<Account> accounts = Flux.zip(Flux.range(0, n), Flux.interval(Duration.ofMillis(100)))
                 .map(T -> T.getT1())
-                .map(i -> new Account(String.valueOf(i))).log();
+                .map(i -> new Account(String.valueOf(i)));
+//        Flux<Account> anotherAccounts = Flux.zip(Flux.range(n, n), Flux.interval(Duration.ofMillis(200)))
+//                .map(T -> new Account(String.valueOf(T.getT1())));
         model.addAttribute("accounts", new ReactiveDataDriverContextVariable(accounts, 10));
+//        model.addAttribute("anothers", new ReactiveDataDriverContextVariable(anotherAccounts, 5));
         return "biglist";
     }
 }
